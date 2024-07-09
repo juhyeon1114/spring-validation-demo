63e041dbcc11a6c3d068f54456f95c24e43af323
juhyeon <juhyeon1114@gmail.com>
2024-07-09 21:30:29 +0900

    init

---
+++ src/test/java/study/spring_validation_demo/SpringValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_validation_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
